0d9bec48ae4db0756a306141ecba3762f8404c65
ohwk123 <seojeong@sookmyung.ac.kr>
2022-10-18 16:22:46 +0900

    02

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 
 int main(void) 
 {
-	int y;
-	int flag = 1;
-	while(flag!=0)
-	{
-		
-		y = 3;
-		flag = 0;
-	}
+	int i;
 	
-	y = 4;
+	for(i=0;i<5;i++)
+	{
+		int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
+	}
+	return 0;
 }
